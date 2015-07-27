1567d37d3a1d0484fa0ba2566d10946cf772ea21
Elkin Urango <elkinurango@localhost.localdomain>
2015-07-27 18:59:00 -0500

    Se edito el package json

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.use(function(err, req, res, next) {
 
 
 // Start the server
-app.set('port', process.env.PORT || 8000);
+app.set('port', process.env.PORT || 57635);
 
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
