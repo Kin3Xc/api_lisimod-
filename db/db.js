c8aafdad9548b168fd9a29c54e264f3dbb22d02d
Elkin Urango <elkinurango@localhost.localdomain>
2015-07-27 21:38:27 -0500

    Heroku

--- db/db.js
+++ db/db.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 // llamo db mongo
 exports.conectar = function(err, res){
 	// mongoose.connect('mongodb://localhost/domisildb');// LOCAL
-	mongoose.connect('mongodb://<domisil>:<domisil123_$#>@ds037617.mongolab.com:37617/domisilapp');// REMOTYE
+	mongoose.connect('mongodb://domisildb:rootshell@ds037617.mongolab.com:37617/domisilapp');// REMOTYE
 
 	if (err) throw err;
 	console.log('CONECTADO A LA BASE DE DATOS DE MONGODB');
 } 
 
-mongodb://<dbuser>:<dbpassword>@ds037617.mongolab.com:37617/domisilapp
+// mongodb://<dbuser>:<dbpassword>@ds037617.mongolab.com:37617/domisilapp
