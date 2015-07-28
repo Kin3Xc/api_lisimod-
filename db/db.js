869d3292a9a6615d349026ef570480f900cdd568
Elkin Urango <elkinurango@localhost.localdomain>
2015-07-27 20:08:07 -0500

    Se agrego URL de DB MongoLab

--- db/db.js
+++ db/db.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 // llamo db mongo
 exports.conectar = function(err, res){
-	mongoose.connect('mongodb://localhost/domisildb');
+	// mongoose.connect('mongodb://localhost/domisildb');// LOCAL
+	mongoose.connect('mongodb://domsil:domisil123_$#@ds037617.mongolab.com:37617/domsilapp');// REMOTYE
+
 	if (err) throw err;
-	console.log('CONECTADO A DB');
-
+	console.log('CONECTADO A LA BASE DE DATOS DE MONGODB');
 } 
-
