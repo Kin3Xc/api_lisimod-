f45dc1b0b812615aacc058c767ffa500428f335e
Elkin Urango <elkinurango@localhost.localdomain>
2015-07-29 10:32:21 -0500

    Edit user

--- auth/auth.js
+++ auth/auth.js
@@ -179,29 +179,42 @@ exports.emailSignup = function(req, res){
 
 
 // prueba libro mean bcrypt - 
-function validateUser(password, user, cb){	
-	if(user === null) { return;} 
+function validateUser(user, password, cb){
 	bcrypt.compare(password, user.password, cb);
 }
 
 // function para ingresar usuario al sistema
 exports.emailLogin = function(req, res){
+	
 	User.findOne({ usuario: req.body.usuario }, function(err, user){
 		if (err) next(err);
-		if(!user) res.status(401).send({message: 'No existe ese usuario'});
+		if(!user) res.json({success: false, message: 'No existe ese usuario'});
 		// aqui viene comprobacion de contraseña bcrypt
-		if (req.body.password === null) { return res.status(401).send({message:'Ingrese su password'})};
-		// if(req.body.password !== null){
-			validateUser(req.body.password, user, function(err, valid){
-				if(err || !valid){ return res.status(401).send({message: 'Contraseña incorrecta', result:valid, pwd:user.password, llega:req.body.password})};
-				// si no hay error y contraseña es igual devuelvo el token con payload
-				console.log(user._id);
-				return res
-					.status(200)
-					.send({ userId: user._id, token: service.createToken(user) });	
-			});
-		// } else{
-		// 	return res.send({message:'llenar el formulario'});
-		// }	
+		validateUser(user, req.body.password, function(err, valid){
+			if(err || !valid){ return res.send(401)}
+			// si no hay error y contraseña es igual devuelvo el token con payload
+			return res
+				.status(200)
+				.send({ token: service.createToken(user) });	
+		});
 	});
+
+	// User.findOne({ usuario: req.body.usuario }, function(err, user){
+	// 	if (err) next(err);
+	// 	if(!user) res.status(401).send({message: 'No existe ese usuario'});
+	// 	// aqui viene comprobacion de contraseña bcrypt
+	// 	if (req.body.password === null) { return res.status(401).send({message:'Ingrese su password'})}
+	// 	if(req.body.password !== null){
+	// 		validateUser(req.body.password, user, function(err, valid){
+	// 			if(err || !valid){ return res.status(401).send({message: 'Contraseña incorrecta'})}
+	// 			// si no hay error y contraseña es igual devuelvo el token con payload
+	// 			console.log(user._id);
+	// 			return res
+	// 				.status(200)
+	// 				.send({ userId: user._id, token: service.createToken(user) });	
+	// 		});
+	// 	} else{
+	// 		return res.send({message:'llenar el formulario'});
+	// 	}	
+	// });
 };
